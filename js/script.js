f73eaa6ebee6fc8cf17cce1d2aa0b7a6f4375e31
niccoloruberti <nikiruberti@gmail.com>
2023-05-17 16:49:40 +0200

    problemi sulla verifica che i dati siano numeri

---
+++ js/script.js
@@ -0,0 +1,28 @@
+let km = parseInt(prompt('Quanti Km deisderi percorrere?'));
+let age = parseInt(prompt('Quanti anni hai?'));
+const tariffaKm = 0.21;
+let costoBiglietto;
+
+console.log(km)
+console.log(age)
+
+//controllo che i dati inseriti siano dei numeri
+if (typeof age != 'number' || typeof km != 'number') {
+    alert('I dati devono essere inseriti come numeri')
+} else {
+    //calcolo il prezzo del biglietto
+    if (age < 18) {
+        //applico uno sconto del 20%
+        costoBiglietto = (km * tariffaKm) - (((km * tariffaKm) / 100) * 20)
+    }
+    else if (age > 65) {
+        //applico uno sconto del 40%
+        costoBiglietto = (km * tariffaKm) - (((km * tariffaKm) / 100) * 40)
+    } else {
+        //applico la tariffa normale
+        costoBiglietto = km * tariffaKm
+    }
+    console.log(costoBiglietto)
+    document.getElementById('costo').innerHTML = `il costo del biglietto è ${costoBiglietto}`
+}
+
